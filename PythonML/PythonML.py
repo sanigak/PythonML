29b5915b1c24fae8c4d055bddbe1f2a9d2339625
Keith Saniga <sanigak@outlook.com>
2019-03-05 08:40:15 -0500

    added some example code

--- PythonML/PythonML.py
+++ PythonML/PythonML.py
@@ -12,3 +12,42 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# shape
+#print(dataset.shape)
+
+
+# head
+#print(dataset.head(20))
+
+# descriptions
+#print(dataset.describe())
+
+
+# class distribution
+#print(dataset.groupby('class').size())
+
+# box and whisker plots
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#plt.show()
+
+# histograms
+#dataset.hist()
+#plt.show()
+
+# scatter plot matrix
+#scatter_matrix(dataset)
+#plt.show()
+
+
+
+
+
+
+
+
