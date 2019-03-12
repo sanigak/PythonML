639ec067f40167f1d1752f4eb4c9130f429901c5
Keith Saniga <sanigak@outlook.com>
2019-03-12 09:53:32 -0400

    examined the creation of validation vs testing data sets more closely

---
+++ PythonML/ValidationDataset.py
@@ -0,0 +1,45 @@
+#For examining the algorithm for separating a validation dataset
+
+
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
+
+
+
+# Split-out validation dataset
+array = dataset.values
+#print(array)
+
+
+    #X is all the "numerical data"
+X = array[:,0:4]
+
+
+    #Y is all the "categories"
+Y = array[:,4]
+
+
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+
